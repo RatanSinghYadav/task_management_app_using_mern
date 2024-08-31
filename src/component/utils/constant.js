8c3ebfbc22cc8b2499d8e37ed7204873ca4b7754
Ratan Singh Yadav <108169130+RatanSinghYadav@users.noreply.github.com>
2024-08-31 12:00:37 +0530

    Update constant.js

--- src/component/utils/constant.js
+++ src/component/utils/constant.js
@@ -1,9 +1,6 @@
 module.exports = Object.freeze({
 
-    // url:"https://task-management-api-ngel.onrender.com",
-    url:"https://fine-pegasus-slightly.ngrok-free.app"
-
-//     url:"https://task-management-api-ngel.onrender.com",
+    url:"https://task-management-api-ngel.onrender.com",
 //     url:"http://localhost:8000"
 
 })
