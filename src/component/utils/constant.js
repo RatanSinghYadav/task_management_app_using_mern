a40d28caac8455e2277f236301e2f8b2cafd3a2f
Ratan Singh Yadav <108169130+RatanSinghYadav@users.noreply.github.com>
2024-10-10 16:03:06 +0530

    Update constant.js

--- src/component/utils/constant.js
+++ src/component/utils/constant.js
@@ -1,6 +1,7 @@
 module.exports = Object.freeze({
 
-    url:"https://task-management-api-ngel.onrender.com",
-//     url:"http://localhost:8000"
+    // url:"https://task-management-api-ngel.onrender.com",
+    // url:"http://localhost:8000",
+    url:"tickedserver-d3d4b7hvgra2axeu.centralindia-01.azurewebsites.net"
 
 })
